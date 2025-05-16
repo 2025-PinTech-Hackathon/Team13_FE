f42c6903d153f48ae02743c36c5a158efba10670
zerow.kim <0wkim@naver.com>
2025-05-16 20:50:13 +0900

    update: 이미지 오류 해결

--- src/pages/how_to_use/Step4.jsx
+++ src/pages/how_to_use/Step4.jsx
@@ -43,7 +43,9 @@ const Step4 = () => {
           </svg>
         </button>
 
+        <div className="flex justify-center items-center w-[80%] aspect-[2/3] object-cover">
         <img src={step4} alt="Step 4" className="w-[73.95833333333333%] h-auto" />
+        </div>
       </div>
 
       {/* 하단 */}
