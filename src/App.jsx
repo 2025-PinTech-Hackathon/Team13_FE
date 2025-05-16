589351cac15b5e52532d8c186d667638ae910a7d
Junhyeok <cjh64866@naver.com>
2025-05-16 18:03:43 +0900

    feat: 계좌 확인 페이지 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./styles/fonts.css";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Landing from "./pages/Landing";
 import SendMoney from "./pages/SendMoney";
-import MyAccount from "./pages/MyAccount";
 import SendMoneyCamera from "./pages/SendMoneyCamera";
 import TransferConfirm from "./pages/TransferConfirm";
 import Password from "./pages/Password";
@@ -13,7 +12,7 @@ import SendMoneyLoading from "./pages/SendMoneyLoading";
 import "./styles/fonts.css";
 import Loading from "./pages/Loading";
 import SendMoneyComplete from "./pages/SendMonyComplete";
-import AcountView from "./pages/AccountView";
+import AccountView from "./pages/AccountView";
 
 function App() {
   return (
@@ -29,25 +28,20 @@ function App() {
             <Route path="/confirm" element={<TransferConfirm />} />
             <Route path="/password" element={<Password />} />
             <Route path="/send_complete" element={<SendMoneyComplete />} />
-            <Route path="/account_view" element={<AcountView />} />
+            <Route path="/account_view" element={<AccountView />} />
             {/* <Route index element={<SendMoneyCamera />} /> */}
 
             <Route path="/loading" element={<Loading />} />
-
           </Route>
 
           {/* 돈 송금하기 */}
           <Route path="/send_money" element={<SendMoney />}></Route>
 
-          {/* 내 계좌 보기 */}
-          <Route path="/my_account" element={<MyAccount />}></Route>
-
-            {/* 송금 컨펌 페이지 */}
-            <Route path="/transfer_confirm" element={<TransferConfirm />}></Route>
+          {/* 송금 컨펌 페이지 */}
+          <Route path="/transfer_confirm" element={<TransferConfirm />}></Route>
 
           {/* 송금 비밀번호 입력 */}
           <Route path="/password" element={<Password />}></Route>
-
         </Routes>
       </div>
     </Router>
--- src/pages/MyAccount.jsx
+++
@@ -1,5 +0,0 @@
-const MyAccount = () => {
-
-}
-
-export default MyAccount;
