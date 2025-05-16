f6899bc2879c16afe83ce4e1f3dacde03a2fe027
zerow.kim <0wkim@naver.com>
2025-05-16 20:05:01 +0900

    update: 송금 완료 페이지 경로 수정

--- src/components/BottomBtn.jsx
+++ src/components/BottomBtn.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
 
-const BottomBtn = ({ prevRoute, nextRoute, leftText = "취소", rightText = "확인" }) => {
+const BottomBtn = ({ prevRoute, nextRoute, leftText = "취소", rightText = "확인", ...props}) => {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full flex max-w-[480px]">
+    <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full flex max-w-[480px]" {...props}>
       <button
         className="flex-1 h-[80px] bg-gray2 text-txt-black Pr_SB_20 pt-1"
         onClick={() => {
