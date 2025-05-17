51dc19dc522e07173ed028b4cae759080c990c50
Junhyeok <cjh64866@naver.com>
2025-05-17 09:03:46 +0900

    fix: key error 수정

--- src/pages/TransferConfirm2.jsx
+++ src/pages/TransferConfirm2.jsx
@@ -8,6 +8,7 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
 const translateToKorean = async (text) => {
